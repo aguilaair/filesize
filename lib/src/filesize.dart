06cc19ddea1334e7ab36df2c9b35effb434a35dc
Win <synwe@yahoo.com>
2019-05-04 21:51:03 +0000

    Small fixes after strict linting

--- lib/src/filesize.dart
+++ lib/src/filesize.dart
@@ -1,49 +1,56 @@
-/// A method returns a human readable string representing a file size
-String filesize(size, [int round = 2]) {
-  int divider = 1024;
+/// A method returns a human readable string representing a file _size
+String filesize(dynamic size, [int round = 2]) {
   /** 
    * [size] can be passed as number or as string
    *
    * the optional parameter [round] specifies the number 
    * of digits after comma/point (default is 2)
    */
+  int divider = 1024;
+  int _size;
   try {
-    size = int.parse(size.toString());
+    _size = int.parse(size.toString());
   } catch (e) {
-    throw ("filesize: can not parse the size parameter: $e");
+    throw ("Filesize: can not parse the size parameter: $e");
   }
 
-  if (size < divider) return "$size B";
+  if (_size < divider) return "$_size B";
 
-  if (size < divider * divider && size % divider == 0)
-    return "${(size / divider).toStringAsFixed(0)} KB";
+  if (_size < divider * divider && _size % divider == 0)
+    return "${(_size / divider).toStringAsFixed(0)} KB";
 
-  if (size < divider * divider)
-    return "${(size / divider).toStringAsFixed(round)} KB";
+  if (_size < divider * divider)
+    return "${(_size / divider).toStringAsFixed(round)} KB";
 
-  if (size < divider * divider * divider && size % divider == 0)
-    return "${(size / (divider * divider)).toStringAsFixed(0)} MB";
+  if (_size < divider * divider * divider && _size % divider == 0)
+    return "${(_size / (divider * divider)).toStringAsFixed(0)} MB";
 
-  if (size < divider * divider * divider)
-    return "${(size / divider / divider).toStringAsFixed(round)} MB";
+  if (_size < divider * divider * divider)
+    return "${(_size / divider / divider).toStringAsFixed(round)} MB";
 
-  if (size < divider * divider * divider * divider && size % divider == 0)
-    return "${(size / (divider * divider * divider)).toStringAsFixed(0)} GB";
+  if (_size < divider * divider * divider * divider && _size % divider == 0)
+    return "${(_size / (divider * divider * divider)).toStringAsFixed(0)} GB";
 
-  if (size < divider * divider * divider * divider)
-    return "${(size / divider / divider / divider).toStringAsFixed(round)} GB";
+  if (_size < divider * divider * divider * divider)
+    return "${(_size / divider / divider / divider).toStringAsFixed(round)} GB";
 
-  if (size < divider * divider * divider * divider * divider &&
-      size % divider == 0)
-    return "${(size / divider / divider / divider / divider).toStringAsFixed(0)} TB";
+  if (_size < divider * divider * divider * divider * divider &&
+      _size % divider == 0) {
+    num r = _size / divider / divider / divider / divider;
+    return "${r.toStringAsFixed(0)} TB";
+  }
 
-  if (size < divider * divider * divider * divider * divider)
-    return "${(size / divider / divider / divider / divider).toStringAsFixed(round)} TB";
+  if (_size < divider * divider * divider * divider * divider) {
+    num r = _size / divider / divider / divider / divider;
+    return "${r.toStringAsFixed(round)} TB";
+  }
 
-  if (size < divider * divider * divider * divider * divider * divider &&
-      size % divider == 0) {
-    return "${(size / divider / divider / divider / divider / divider).toStringAsFixed(0)} PB";
+  if (_size < divider * divider * divider * divider * divider * divider &&
+      _size % divider == 0) {
+    num r = _size / divider / divider / divider / divider / divider;
+    return "${r.toStringAsFixed(0)} PB";
   } else {
-    return "${(size / divider / divider / divider / divider / divider).toStringAsFixed(round)} PB";
+    num r = _size / divider / divider / divider / divider / divider;
+    return "${r.toStringAsFixed(round)} PB";
   }
 }
